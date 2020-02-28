ffc56e2f849988c9f8a3056f2d197dcc28c765c4
sandrine <sandrinezocca@gmail.com>
2020-02-28 15:04:07 +0100

    define the cocktail model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktail } from './cocktail';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Cocktail';
+
+  cocktail = new Cocktail("mojito");
 }
